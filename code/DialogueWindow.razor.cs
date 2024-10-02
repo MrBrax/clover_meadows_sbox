b739d0fdefab8de02fe77a02666ad6fa0a0d6075
Braxen <braxen@braxnet.org>
2024-10-02 23:39:30 +0200

    speech stuff

--- code/DialogueWindow.razor.cs
+++ code/DialogueWindow.razor.cs
@@ -10,9 +10,20 @@ public partial class DialogueWindow
 	public Dictionary<string, object> Data { get; set; } =
 		new() { { "test", 123 }, { "money", 100 }, { "price", 200 }, };
 
-	public Dialogue.DialogueChoice CurrentChoice { get; set; }
-	public Dialogue.DialogueNode CurrentNode { get; set; }
-	public int CurrentNodeIndex { get; set; }
+	public List<Dialogue.DialogueNode> CurrentNodeList;
+
+	public Dialogue.DialogueChoice CurrentChoice;
+
+	// public Dialogue.DialogueNode CurrentNode { get; set; }
+	public int CurrentNodeIndex;
+
+	public Dialogue.DialogueNode CurrentNode
+	{
+		get => CurrentNodeList[CurrentNodeIndex];
+		// set => CurrentNodeIndex = Dialogue.Nodes.IndexOf( value );
+	}
+	
+	public bool IsOnLastNode => CurrentNodeIndex == CurrentNodeList.Count - 1;
 
 	public string Text { get; set; } = "";
 	public string Name => CurrentNode?.Speaker;
@@ -20,6 +31,7 @@ public partial class DialogueWindow
 	private int _textIndex;
 	private string _textTarget;
 	private TimeSince _lastLetter;
+	private bool _skipped;
 
 
 	protected override void OnStart()
@@ -28,7 +40,8 @@ public partial class DialogueWindow
 
 		Data = new Dictionary<string, object> { { "test", 123 }, { "money", 100 }, { "price", 200 }, };
 
-		CurrentNode = Dialogue.Nodes[0];
+		CurrentNodeList = Dialogue.Nodes;
+		CurrentNodeIndex = 0;
 		CurrentNode.OnEnter?.Invoke( this, Data, null, null, CurrentNode, null );
 		Read();
 	}
@@ -77,7 +90,7 @@ public partial class DialogueWindow
 	/// <param name="id"></param>
 	public void JumpToId( string id )
 	{
-		Dialogue.DialogueNode FindNode( List<Dialogue.DialogueNode> nodes )
+		/*Dialogue.DialogueNode FindNode( List<Dialogue.DialogueNode> nodes )
 		{
 			foreach ( var node in nodes )
 			{
@@ -103,8 +116,53 @@ public partial class DialogueWindow
 		{
 			CurrentNode?.OnExit?.Invoke( this, Data, null, null, CurrentNode, null );
 			CurrentNode = node;
+			CurrentNodeList = null;
 			CurrentNode.OnEnter?.Invoke( this, Data, null, null, CurrentNode, null );
 			Read();
+		}
+		else
+		{
+			Log.Warning( $"Could not find node with id {id}" );
+		}*/
+
+		( Dialogue.DialogueNode node, List<Dialogue.DialogueNode> list ) FindNode( List<Dialogue.DialogueNode> nodes )
+		{
+			foreach ( var node in nodes )
+			{
+				if ( node.Id == id )
+					return (node, nodes);
+
+				if ( node.Choices.Count > 0 )
+				{
+					foreach ( var choice in node.Choices )
+					{
+						var found = FindNode( choice.Nodes );
+						if ( found.node != null )
+							return found;
+					}
+				}
+			}
+
+			return (null, null);
+		}
+
+		var (node, list) = FindNode( Dialogue.Nodes );
+
+		if ( node != null )
+		{
+			CurrentNode?.OnExit?.Invoke( this, Data, null, null, CurrentNode, null );
+			CurrentNodeList = list;
+			CurrentNodeIndex = list.IndexOf( node );
+			Log.Info( $"Jumped to node {node.Id}, index {CurrentNodeIndex}/{list.Count}" );
+			if ( CurrentNode != null )
+			{
+				CurrentNode.OnEnter?.Invoke( this, Data, null, null, CurrentNode, null );
+				Read();
+			}
+			else
+			{
+				Log.Error( "No nodes found for choice" );
+			}
 		}
 		else
 		{
@@ -118,6 +176,8 @@ public partial class DialogueWindow
 		Text = "";
 		_textIndex = 0;
 		_textTarget = ParseVariables( CurrentNode.Text );
+		// _skipped = false;
+		// Panel.FlashClass( "noclick", 0.1f );
 	}
 
 	private string ParseVariables( string text )
@@ -148,20 +208,77 @@ public partial class DialogueWindow
 			if ( _textIndex < _textTarget.Length )
 			{
 				Text += _textTarget[_textIndex];
+				OnLetterTyped( _textTarget[_textIndex] );
 				_textIndex++;
 			}
 		}
 	}
+	
+	private void OnLetterTyped( char letter )
+	{
+		
+		switch ( letter )
+		{
+			case '1':
+				letter = 'o';
+				break;
+			case '2':
+				letter = 't';
+				break;
+			case '3':
+				letter = 't';
+				break;
+			case '4':
+				letter = 'f';
+				break;
+			case '5':
+				letter = 'f';
+				break;
+			case '6':
+				letter = 's';
+				break;
+			case '7':
+				letter = 's';
+				break;
+			case '8':
+				letter = 'e';
+				break;
+			case '9':
+				letter = 'n';
+				break;
+			case '0':
+				letter = 'z';
+				break;
+		}
+
+		var s = SoundFile.Load( "sounds/speech/alphabet/" + letter.ToString().ToUpper() + ".wav" );
+		
+		var h = Sound.PlayFile( s );
+		h.Pitch = Random.Shared.Float( 1.9f, 2.1f );
+
+	}
 
 	private void OnClick()
 	{
+		if ( _textIndex < 2 ) return;
+		
 		// If we're still typing, finish the text
-		/*if ( Text.Length < _textTarget.Length )
+		if ( Text.Length < _textTarget.Length )
 		{
-			Log.Info( "Finish the text" );
+			Log.Info( "Skipping text" );
+			// _skipped = true;
+			_textIndex = _textTarget.Length;
 			Text = _textTarget;
 			return;
-		}*/
+		}
+
+		// if we're at the last node, close the window
+		if ( IsOnLastNode && CurrentNode.Choices.Count == 0 )
+		{
+			Log.Info( "Closing window" );
+			Enabled = false;
+			return;
+		}
 	}
 
 	private void OnChoice( Dialogue.DialogueChoice choice )
@@ -175,9 +292,17 @@ public partial class DialogueWindow
 		}
 		else
 		{
-			CurrentChoice = choice;
+			if ( choice.Nodes.Count == 0 )
+			{
+				Log.Warning( "No nodes found for choice" );
+				return;
+			}
+			
 			CurrentNode.OnExit?.Invoke( this, Data, null, null, CurrentNode, null );
-			CurrentNode = choice.Nodes.FirstOrDefault();
+			CurrentNodeList = choice.Nodes;
+			CurrentNodeIndex = 0;
+			// CurrentChoice = choice;
+				
 			if ( CurrentNode != null )
 			{
 				CurrentNode.OnEnter?.Invoke( this, Data, null, null, CurrentNode, null );
