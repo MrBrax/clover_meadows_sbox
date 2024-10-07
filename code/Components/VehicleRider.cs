99cb9d9fbd26c06971ae74b8fd1bdb0360081f2e
CATERPILLAR\Braxen <braxen@braxnet.org>
2024-10-07 14:59:53 +0200

    Create VehicleRider.cs

---
+++ code/Components/VehicleRider.cs
@@ -0,0 +1,19 @@
+﻿using Clover.Vehicles;
+
+namespace Clover.Components;
+
+public class VehicleRider : Component
+{
+	
+	public BaseVehicle Vehicle { get; set; }
+
+	public void OnEnterVehicle( BaseVehicle baseVehicle, int seatIndex )
+	{
+		Vehicle = baseVehicle;
+	}
+	
+	public void OnExitVehicle()
+	{
+		Vehicle = null;
+	}
+}
