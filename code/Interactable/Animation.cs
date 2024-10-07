e60658081cdad893a452c6eb1d83fd81368a7b61
Braxen <braxen@braxnet.org>
2024-10-07 18:28:49 +0200

    add simple interactable animation component

---
+++ code/Interactable/Animation.cs
@@ -0,0 +1,24 @@
+﻿using Clover.Player;
+
+namespace Clover.Interactable;
+
+[Category( "Clover/Interactable" )]
+[Icon( "directions_run" )]
+public class Animation : Component, IInteract
+{
+	
+	[Property] public SkinnedModelRenderer Renderer { get; set; }
+	
+	[Property] public string AnimationName { get; set; }
+
+	public void StartInteract( PlayerCharacter player )
+	{
+		Renderer.Set( AnimationName, true );
+	}
+
+	public void FinishInteract( PlayerCharacter player )
+	{
+		
+	}
+	
+}
