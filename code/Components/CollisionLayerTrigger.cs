4056a7c0d3d0e6ae60a70c0ea59c3c81f88d5ddf
Braxen <braxen@braxnet.org>
2024-10-07 21:05:20 +0200

    bridge

---
+++ code/Components/CollisionLayerTrigger.cs
@@ -0,0 +1,32 @@
+﻿namespace Clover.Components;
+
+public class CollisionLayerTrigger : Component, Component.ITriggerListener
+{
+	[Property] public string Layer { get; set; }
+
+	void ITriggerListener.OnTriggerEnter( Collider other )
+	{
+		Log.Info( $"OnTriggerEnter {other.GameObject}" );
+		if ( !other.GameObject.IsValid() )
+		{
+			Log.Warning( "Invalid GameObject" );
+			return;
+		}
+
+		Log.Info($"Adding {Layer} to {other.GameObject}");
+		other.GameObject.Tags.Add( Layer );
+	}
+
+	void ITriggerListener.OnTriggerExit( Collider other )
+	{
+		Log.Info( $"OnTriggerExit {other.GameObject}" );
+		if ( !other.GameObject.IsValid() )
+		{
+			Log.Warning( "Invalid GameObject" );
+			return;
+		}
+
+		Log.Info($"Removing {Layer} from {other.GameObject}");
+		other.GameObject.Tags.Remove( Layer );
+	}
+}
