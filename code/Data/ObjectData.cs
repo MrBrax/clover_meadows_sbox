1b4b95e164750e7e2b62fc3f1f5856a83c967082
Braxen <braxen@braxnet.org>
2024-10-07 22:24:38 +0200

    soccer ball

---
+++ code/Data/ObjectData.cs
@@ -0,0 +1,12 @@
+﻿namespace Clover.Data;
+
+[GameResource("ObjectData", "odata", "Object Data")]
+public class ObjectData : GameResource // TODO: better name
+{
+	
+	[Property] public string Name { get; set; }
+	[Property, TextArea] public string Description { get; set; }
+	
+	[Property] public GameObject Prefab { get; set; }
+	
+}
---
+++ code/Items/WorldObject.cs
@@ -0,0 +1,31 @@
+﻿using Clover.Data;
+using Clover.Inventory;
+using Clover.Player;
+
+namespace Clover.Items;
+
+public class WorldObject : Component, IPickupable
+{
+	[RequireComponent] public WorldLayerObject WorldLayerObject { get; set; }
+	
+	private string _prefab;
+
+	[Property, ResourceType( "prefab" )]
+	public string Prefab
+	{
+		get { return !string.IsNullOrEmpty( _prefab ) ? _prefab : GameObject.PrefabInstanceSource; }
+		set { _prefab = value; }
+	}
+
+	[Property] public ObjectData ObjectData { get; set; }
+	
+	public bool CanPickup( PlayerCharacter player )
+	{
+		throw new System.NotImplementedException();
+	}
+
+	public void OnPickup( PlayerCharacter player )
+	{
+		throw new System.NotImplementedException();
+	}
+}
