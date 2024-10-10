53b79bc4699e4396513bf5ac2d51df212f2561e7
Braxen <braxen@braxnet.org>
2024-10-10 22:02:47 +0200

    soccer ball sound and kick force

---
+++ code/Items/SoccerBall.cs
@@ -0,0 +1,29 @@
+﻿using Clover.Player;
+
+namespace Clover.Items;
+
+public class SoccerBall : Component, Component.ICollisionListener
+{
+	[Property] public SoundEvent KickSound { get; set; }
+
+	public void OnCollisionStart( Collision collision )
+	{
+
+		if ( collision.Other.GameObject == null ||
+		     collision.Other.GameObject.Components.Get<PlayerCharacter>() == null )
+		{
+			return;
+		}
+		
+		// Log.Info( $"SoccerBall collided with {collision.Other.GameObject}" );
+		
+		Components.Get<Rigidbody>().ApplyImpulseAt( collision.Contact.Point, collision.Contact.Normal * 5000.0f );
+
+		GameObject.PlaySound( KickSound );
+	}
+
+	/*public void OnCollisionUpdate( Collision collision )
+	{
+		Log.Info( "SoccerBall is colliding with something" );
+	}*/
+}
