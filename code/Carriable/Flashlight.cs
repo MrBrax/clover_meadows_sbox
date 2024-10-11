0383a781cef4f7a3fb044c27cf787c483fb150b4
Braxen <braxen@braxnet.org>
2024-10-11 19:25:49 +0200

    flashlight toggle

--- code/Carriable/Flashlight.cs
+++ code/Carriable/Flashlight.cs
@@ -3,4 +3,19 @@
 public class Flashlight : BaseCarriable
 {
 	
+	// private bool _enabled;
+	
+	[Property] public Light Light { get; set; }
+
+	public override void OnUseDown()
+	{
+		base.OnUseDown();
+		
+		if ( !CanUse() ) return;
+
+		NextUse = 0.1f;
+		
+		// _enabled = !_enabled;
+		Light.Enabled = !Light.Enabled;
+	}
 }
