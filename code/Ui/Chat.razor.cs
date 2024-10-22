50cdbf2e3f735067eb5e332321b9d9349cc0a228
Braxen <braxen@braxnet.org>
2024-10-22 18:55:05 +0200

    simple chat

---
+++ code/Ui/Chat.razor.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Clover.Player;
+using Sandbox.UI;
+
+namespace Clover;
+
+public partial class Chat
+{
+	public static Chat Instance { get; private set; }
+
+	public List<ChatMessage> Messages { get; set; } = new();
+
+	public List<ChatMessage> DistantMessages => Messages.Where( x => !x.IsNearby ).ToList();
+
+	private bool ShouldShowMessages => DistantMessages.Count > 0 || ShouldShowInput;
+
+	private bool ShouldShowInput;
+
+	public record ChatMessage
+	{
+		public string Text { get; init; }
+		public ulong Author { get; init; }
+		public string PlayerId { get; init; }
+		public DateTime Time { get; init; }
+
+		public string Name => IsYou ? "You" : (Player.IsValid() ? Player.PlayerName : "Unknown");
+
+		public bool IsYou => PlayerId == PlayerCharacter.Local.PlayerId;
+
+		public PlayerCharacter Player => Game.ActiveScene.GetAllComponents<PlayerCharacter>()
+			.FirstOrDefault( x => x.PlayerId == PlayerId /*|| x.Network.Owner.SteamId == Author*/ );
+
+		public bool IsNearby => Player.IsValid() && PlayerCharacter.Local.IsValid() &&
+		                        PlayerCharacter.Local.WorldPosition.Distance( Player.WorldPosition ) < 300;
+	}
+
+
+	private string MessageText { get; set; } = "";
+
+	private TextEntry InputBox;
+
+
+	protected override int BuildHash() =>
+		System.HashCode.Combine( Messages, DistantMessages, ShouldShowInput, MessageText );
+
+	protected override void OnAwake()
+	{
+		Instance = this;
+	}
+
+	protected override void OnDestroy()
+	{
+		Instance = null;
+	}
+
+	protected override void OnFixedUpdate()
+	{
+		if ( Input.EscapePressed )
+		{
+			ShouldShowInput = false;
+			StateHasChanged();
+		}
+
+		if ( Input.Pressed( "Chat" ) )
+		{
+			if ( !ShouldShowInput )
+			{
+				Log.Info( "Enable Chat" );
+				ShouldShowInput = true;
+				StateHasChanged();
+				InputBox?.Focus();
+				return;
+			}
+
+			SendMessage();
+		}
+
+		if ( Messages.RemoveAll( x => x.Time < DateTime.Now.AddSeconds( -30 ) ) > 0 )
+		{
+			StateHasChanged();
+		}
+	}
+
+	private void SendMessage()
+	{
+		if ( !string.IsNullOrWhiteSpace( MessageText ) )
+		{
+			Log.Info( "Send Message" );
+			AddMessage( MessageText, PlayerCharacter.Local.PlayerId );
+			MessageText = "";
+		}
+
+		ShouldShowInput = false;
+		StateHasChanged();
+	}
+
+	[Broadcast]
+	public void AddMessage( string message, string playerId )
+	{
+		Messages.Add( new ChatMessage()
+		{
+			Text = message, Author = Rpc.Caller.SteamId, Time = DateTime.Now, PlayerId = playerId
+		} );
+		StateHasChanged();
+	}
+}
---
+++ code/Ui/ChatBubbles.razor.cs
@@ -0,0 +1,13 @@
+﻿using Clover.Player;
+
+namespace Clover;
+
+public partial class ChatBubbles
+{
+	private PlayerCharacter Player => GameObject.Parent.GetComponent<PlayerCharacter>();
+
+	private List<Chat.ChatMessage> Messages =>
+		Chat.Instance?.Messages.Where( x => x.PlayerId == Player.PlayerId ).ToList() ?? new();
+
+	protected override int BuildHash() => System.HashCode.Combine( Messages );
+}
