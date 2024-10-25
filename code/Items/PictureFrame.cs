aac0f17c3537b7226fd897e18c687103c871896f
Braxen <braxen@braxnet.org>
2024-10-26 01:26:12 +0200

    picture frame

---
+++ code/Items/PictureFrame.cs
@@ -0,0 +1,17 @@
+﻿using Clover.Persistence;
+
+namespace Clover.Items;
+
+public class PictureFrame : DecalItem, IPersistent
+{
+	[RequireComponent] public WorldItem WorldItem { get; private set; }
+	[Property] public ModelRenderer ModelRenderer { get; set; }
+
+
+	public override void OnMaterialUpdate( Material material )
+	{
+		base.OnMaterialUpdate( material );
+
+		ModelRenderer.SetMaterialOverride( material, "image" );
+	}
+}
