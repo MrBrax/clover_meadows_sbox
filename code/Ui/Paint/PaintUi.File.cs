6dcfccc04504b81b15331e9b17cb582702dc7ffb
Braxen <braxen@braxnet.org>
2024-10-25 20:36:58 +0200

    split more

---
+++ code/Ui/Paint/PaintUi.File.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.IO;
+using Clover.Player;
+using Clover.Utilities;
+
+namespace Clover.Ui;
+
+public partial class PaintUi
+{
+	private void PopulateDecals()
+	{
+		Decals.Clear();
+
+		var files = FileSystem.Data.FindFile( "decals", "*.decal" );
+		foreach ( var file in files )
+		{
+			Decals.DecalData decal;
+			try
+			{
+				decal = Utilities.Decals.ReadDecal( $"decals/{file}" );
+			}
+			catch ( Exception e )
+			{
+				Log.Error( e.Message );
+				continue;
+			}
+
+			Decals.Add( new DecalEntry { Decal = decal, ResourcePath = $"decals/{file}" } );
+		}
+	}
+
+
+	private void LoadDecal( string path )
+	{
+		Log.Info( $"Loading decal {path}" );
+
+		Decals.DecalData decal;
+
+		try
+		{
+			decal = Utilities.Decals.ReadDecal( path );
+		}
+		catch ( Exception e )
+		{
+			Log.Error( e.Message );
+			return;
+		}
+
+		CurrentName = decal.Name;
+		PaletteName = decal.Palette;
+		CurrentDecalData = decal;
+		CurrentFileName = Path.GetFileNameWithoutExtension( path );
+		DrawTexture.Update( decal.Texture.GetPixels(), 0, 0, decal.Width, decal.Height );
+		DrawTextureData = decal.Image;
+	}
+
+	private void PopulateImages()
+	{
+		Images.Clear();
+
+		var files = FileSystem.Data.FindFile( "decals", "*.png" );
+		foreach ( var file in files )
+		{
+			var texture = Texture.Load( FileSystem.Data, $"decals/{file}" );
+			Images.Add( texture );
+		}
+
+		Log.Info( $"Loaded {Images.Count} images" );
+	}
+
+	private void LoadImage( Texture texture )
+	{
+		Log.Info( $"Loading image {texture.ResourcePath}, {texture.Width}x{texture.Height}" );
+
+		// resize image to 32x32
+		var resizedTexture = Texture.Create( TextureSize, TextureSize ).WithDynamicUsage().Finish();
+		resizedTexture.Update( texture.GetPixels(), 0, 0, TextureSize, TextureSize );
+
+		// pick best fitting colors
+		var pixels = resizedTexture.GetPixels();
+
+		var newBytes = new byte[TextureSize * TextureSize];
+
+		for ( var i = 0; i < pixels.Length; i++ )
+		{
+			var pixel = pixels[i];
+			var closestColor = Utilities.Decals.GetClosestPaletteColor( Palette.ToArray(), pixel );
+			// pixels[i] = Palette[closestColor];
+			newBytes[i] = (byte)closestColor;
+		}
+
+		DrawTextureData = newBytes;
+		PushByteDataToTexture();
+	}
+
+	private void Save()
+	{
+		if ( string.IsNullOrEmpty( CurrentFileName ) )
+		{
+			PlayerCharacter.Local.Notify( Notifications.NotificationType.Error, "No file name" );
+			return;
+		}
+
+		if ( string.IsNullOrEmpty( CurrentName ) )
+		{
+			PlayerCharacter.Local.Notify( Notifications.NotificationType.Error, "No name" );
+			return;
+		}
+
+		Stream stream;
+
+		try
+		{
+			stream = FileSystem.Data.OpenWrite( $"decals/{CurrentFileName}.decal" );
+		}
+		catch ( Exception e )
+		{
+			Log.Error( e.Message );
+			PlayerCharacter.Local.Notify( Notifications.NotificationType.Error, "Failed to save file" );
+			return;
+		}
+
+		Utilities.Decals.WriteDecal( stream,
+			new Decals.DecalData
+			{
+				Width = DrawTexture.Width,
+				Height = DrawTexture.Height,
+				Name = CurrentName,
+				Palette = PaletteName,
+				Image = DrawTextureData,
+				Author = (ulong)Game.SteamId,
+				AuthorName = Connection.Local.DisplayName,
+				Created = CurrentDecalData.Created == DateTime.MinValue ? DateTime.Now : CurrentDecalData.Created,
+				Modified = DateTime.Now,
+			} );
+
+		stream.Close();
+
+		PopulateDecals();
+
+		Scene.RunEvent<IPaintEvent>( x => x.OnFileSaved( $"decals/{CurrentFileName}.decal" ) );
+	}
+}
--- code/Ui/Paint/PaintUi.razor.cs
+++ code/Ui/Paint/PaintUi.razor.cs
@@ -235,92 +235,6 @@ public partial class PaintUi
 		PreviewTexture = Texture.Create( TextureSize, TextureSize ).WithDynamicUsage().Finish();
 	}
 
-	private void PopulateDecals()
-	{
-		Decals.Clear();
-
-		var files = FileSystem.Data.FindFile( "decals", "*.decal" );
-		foreach ( var file in files )
-		{
-			Decals.DecalData decal;
-			try
-			{
-				decal = Utilities.Decals.ReadDecal( $"decals/{file}" );
-			}
-			catch ( Exception e )
-			{
-				Log.Error( e.Message );
-				continue;
-			}
-
-			Decals.Add( new DecalEntry { Decal = decal, ResourcePath = $"decals/{file}" } );
-		}
-	}
-
-
-	private void LoadDecal( string path )
-	{
-		Log.Info( $"Loading decal {path}" );
-
-		Decals.DecalData decal;
-
-		try
-		{
-			decal = Utilities.Decals.ReadDecal( path );
-		}
-		catch ( Exception e )
-		{
-			Log.Error( e.Message );
-			return;
-		}
-
-		CurrentName = decal.Name;
-		PaletteName = decal.Palette;
-		CurrentDecalData = decal;
-		CurrentFileName = Path.GetFileNameWithoutExtension( path );
-		DrawTexture.Update( decal.Texture.GetPixels(), 0, 0, decal.Width, decal.Height );
-		DrawTextureData = decal.Image;
-	}
-
-	private void PopulateImages()
-	{
-		Images.Clear();
-
-		var files = FileSystem.Data.FindFile( "decals", "*.png" );
-		foreach ( var file in files )
-		{
-			var texture = Texture.Load( FileSystem.Data, $"decals/{file}" );
-			Images.Add( texture );
-		}
-
-		Log.Info( $"Loaded {Images.Count} images" );
-	}
-
-	private void LoadImage( Texture texture )
-	{
-		Log.Info( $"Loading image {texture.ResourcePath}, {texture.Width}x{texture.Height}" );
-
-		// resize image to 32x32
-		var resizedTexture = Texture.Create( TextureSize, TextureSize ).WithDynamicUsage().Finish();
-		resizedTexture.Update( texture.GetPixels(), 0, 0, TextureSize, TextureSize );
-
-		// pick best fitting colors
-		var pixels = resizedTexture.GetPixels();
-
-		var newBytes = new byte[TextureSize * TextureSize];
-
-		for ( var i = 0; i < pixels.Length; i++ )
-		{
-			var pixel = pixels[i];
-			var closestColor = Utilities.Decals.GetClosestPaletteColor( Palette.ToArray(), pixel );
-			// pixels[i] = Palette[closestColor];
-			newBytes[i] = (byte)closestColor;
-		}
-
-		DrawTextureData = newBytes;
-		PushByteDataToTexture();
-	}
-
 
 	private void SetColor( PanelEvent ev, int index )
 	{
@@ -725,54 +639,6 @@ public partial class PaintUi
 		PreviewTexture.Update( colors, x, y, width, height );
 	}
 
-	private void Save()
-	{
-		if ( string.IsNullOrEmpty( CurrentFileName ) )
-		{
-			PlayerCharacter.Local.Notify( Notifications.NotificationType.Error, "No file name" );
-			return;
-		}
-
-		if ( string.IsNullOrEmpty( CurrentName ) )
-		{
-			PlayerCharacter.Local.Notify( Notifications.NotificationType.Error, "No name" );
-			return;
-		}
-
-		Stream stream;
-
-		try
-		{
-			stream = FileSystem.Data.OpenWrite( $"decals/{CurrentFileName}.decal" );
-		}
-		catch ( Exception e )
-		{
-			Log.Error( e.Message );
-			PlayerCharacter.Local.Notify( Notifications.NotificationType.Error, "Failed to save file" );
-			return;
-		}
-
-		Utilities.Decals.WriteDecal( stream,
-			new Decals.DecalData
-			{
-				Width = DrawTexture.Width,
-				Height = DrawTexture.Height,
-				Name = CurrentName,
-				Palette = PaletteName,
-				Image = DrawTextureData,
-				Author = (ulong)Game.SteamId,
-				AuthorName = Connection.Local.DisplayName,
-				Created = CurrentDecalData.Created == DateTime.MinValue ? DateTime.Now : CurrentDecalData.Created,
-				Modified = DateTime.Now,
-			} );
-
-		stream.Close();
-
-		PopulateDecals();
-
-		Scene.RunEvent<IPaintEvent>( x => x.OnFileSaved( $"decals/{CurrentFileName}.decal" ) );
-	}
-
 	private void Clear()
 	{
 		DrawTexture.Update( Palette[RightPaletteIndex], new Rect( 0, 0, DrawTexture.Width, DrawTexture.Height ) );
@@ -802,14 +668,20 @@ public partial class PaintUi
 
 		UpdateCanvas();
 
-		if ( target != default )
+		/*if ( target != default )
 		{
-			var scroll = CanvasContainer.ScrollOffset;
-			var targetPosition = target * CanvasZoom;
-			var targetOffset = targetPosition - scroll;
-			CanvasContainer.Style.Top = Length.Pixels( -targetOffset.y );
-			CanvasContainer.Style.Left = Length.Pixels( -targetOffset.x );
-		}
+			var canvasSquarePositionX = CanvasSquare.Style.Left.GetValueOrDefault().GetPixels( 1 );
+			var canvasSquarePositionY = CanvasSquare.Style.Top.GetValueOrDefault().GetPixels( 1 );
+
+			var offset = new Vector2( target.x - canvasSquarePositionX, target.y - canvasSquarePositionY );
+			SetCanvasSquareOffset( offset );
+		}*/
+	}
+
+	private void SetCanvasSquareOffset( Vector2 offset )
+	{
+		CanvasSquare.Style.Top = Length.Pixels( offset.y );
+		CanvasSquare.Style.Left = Length.Pixels( offset.x );
 	}
 
 	private void ZoomIn()
