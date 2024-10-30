4a048531f0d3006ec4b48c61f46f057050b45d40
Braxen <braxen@braxnet.org>
2024-10-30 09:41:05 +0100

    private setters on player for less actiongraph bloat

--- code/Player/PlayerCharacter.RequiredComponents.cs
+++ code/Player/PlayerCharacter.RequiredComponents.cs
@@ -5,17 +5,17 @@ namespace Clover.Player;
 
 public sealed partial class PlayerCharacter
 {
-	[RequireComponent] public WorldLayerObject WorldLayerObject { get; set; }
-	[RequireComponent] public CharacterController CharacterController { get; set; }
-	[RequireComponent] public PlayerController PlayerController { get; set; }
-	[RequireComponent] public PlayerInteract PlayerInteract { get; set; }
+	[RequireComponent] public WorldLayerObject WorldLayerObject { get; private set; }
+	[RequireComponent] public CharacterController CharacterController { get; private set; }
+	[RequireComponent] public PlayerController PlayerController { get; private set; }
+	[RequireComponent] public PlayerInteract PlayerInteract { get; private set; }
 
 	[RequireComponent, Icon( "inventory" )]
-	public Inventory.Inventory Inventory { get; set; }
+	public Inventory.Inventory Inventory { get; private set; }
 
-	[RequireComponent] public Equips Equips { get; set; }
-	[RequireComponent] public CameraController CameraController { get; set; }
-	[RequireComponent] public CloverBalanceController CloverBalanceController { get; set; }
-	[RequireComponent] public VehicleRider VehicleRider { get; set; }
-	[RequireComponent] public ItemPlacer ItemPlacer { get; set; }
+	[RequireComponent] public Equips Equips { get; private set; }
+	[RequireComponent] public CameraController CameraController { get; private set; }
+	[RequireComponent] public CloverBalanceController CloverBalanceController { get; private set; }
+	[RequireComponent] public VehicleRider VehicleRider { get; private set; }
+	[RequireComponent] public ItemPlacer ItemPlacer { get; private set; }
 }
